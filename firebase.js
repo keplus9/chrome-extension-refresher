3ed6ffc03c6f40d7336285de59e2d216bc4fd4b0
Volodimir Sivernyuk <volodimirsv525@gmail.com>
2020-05-21 15:53:03 -0400

    Init

---
+++ firebase.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+  apiKey: "<apiKey>",
+  authDomain: "<authDomain>",
+  databaseURL: "<databaseURL>",
+  projectId: "<projectId>",
+  storageBucket: "<storageBucket>",
+  messagingSenderId: "<messagingSenderId>",
+  appId: "<appId>",
+  measurementId: "<measurementId>"
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
+
+
+
+
